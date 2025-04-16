17efead2e47e37e23f79703ea09f5205794c5753
Mohammadvaziripoor <93192486+Mohammadvaziripoor@users.noreply.github.com>
2025-04-16 23:05:32 +0330

    Add files via upload

---
+++ Mod.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int a = 17;
+    int b = 5;
+
+    int result = a % b;
+
+    std::cout << "Modulus of " << a << " % " << b << " = " << result << std::endl;
+
+    return 0;
+}
